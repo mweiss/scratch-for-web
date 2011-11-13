d5391adf9ed6ee662ea0b6b859f75a2126f2786c
Michael Weiss <michael.josh.weiss@gmail.com>
2011-11-12 16:09:57 -0800

    first commit

---
+++ src/scratch-for-web-loader/build_tmp/scratch-for-web-loader-min.js
@@ -0,0 +1,1 @@
+YUI().use(function(B){var A={groups:{"clicrdv":{base:"../../../build/",combine:false,modules:{}}}};if(typeof YUI_config==="undefined"){YUI_config={groups:{}};}B.mix(YUI_config.groups,A.groups);});
